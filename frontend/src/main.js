52ef5949359d09a09d2de46cc3a4ecc8983fcc6c
Samuel <samuelumoren365@gmail.com>
2024-05-21 15:04:26 +0100

    made revisions

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,6 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
-import 'toastr/build/toastr.min.css';
 
 const app = createApp(App);
 app.use(router).mount('#app');
